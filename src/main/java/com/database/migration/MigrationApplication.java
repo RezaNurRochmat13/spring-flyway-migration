30a73d5c127cf301e407b5399157a401cb56412b
Reja <rezanurrochmat@gmail.com>
2022-01-27 14:28:23 +0700

    Add logger zero config

--- src/main/java/com/database/migration/MigrationApplication.java
+++ src/main/java/com/database/migration/MigrationApplication.java
@@ -1,16 +1,18 @@
 package com.database.migration;
 
-import com.database.migration.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MigrationApplication {
-	@Autowired
-	private ProductRepository productRepository;
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MigrationApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(MigrationApplication.class, args);
+
+		LOGGER.warn("Example Warn Level");
 	}
 }
