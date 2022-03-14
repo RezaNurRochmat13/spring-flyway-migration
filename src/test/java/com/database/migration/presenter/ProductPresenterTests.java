ba0dbda6a87658ea69751db813dd67b4a32244fc
Reja <rezanurrochmat@gmail.com>
2022-03-14 14:27:42 +0700

    Add unit test on product endpoint

--- src/test/java/com/database/migration/presenter/ProductPresenterTests.java
+++ src/test/java/com/database/migration/presenter/ProductPresenterTests.java
@@ -3,12 +3,13 @@ package com.database.migration.presenter;
 import com.database.migration.entity.Product;
 import com.database.migration.repository.ProductRepository;
 import com.database.migration.service.ProductServiceImpl;
+import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,12 +20,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
 @AutoConfigureMockMvc
 public class ProductPresenterTests {
     @Autowired
@@ -98,6 +100,40 @@ public class ProductPresenterTests {
 
         // Assertion
         assertEquals(200, response.getResponse().getStatus());
+    }
+
+    @Test
+    public void testCreateProductWithPayload() throws Exception {
+        JSONObject payload = new JSONObject();
+        payload.put("name", "Indomilk");
+        payload.put("qty", new Random().nextInt());
+        payload.put("price", "1500");
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/api/v1/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload.toJSONString());
+
+        MvcResult response = mockMvc
+                .perform(requestBuilder)
+                .andReturn();
+
+        // Assertion
+        assertEquals(200, response.getResponse().getStatus());
+    }
+
+    @Test
+    public void testCreateProductWithoutPayload() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/api/v1/products")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult response = mockMvc
+                .perform(requestBuilder)
+                .andReturn();
+
+        // Assertion
+        assertEquals(400, response.getResponse().getStatus());
     }
 
 }
