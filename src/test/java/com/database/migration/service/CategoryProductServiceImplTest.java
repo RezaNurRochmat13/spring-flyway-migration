39b32069197accd3fb05df6f27c4462ae67da58b
Reja <rezanurrochmat@gmail.com>
2022-08-08 15:32:23 +0700

    add unit test for find by category by id

--- src/test/java/com/database/migration/service/CategoryProductServiceImplTest.java
+++ src/test/java/com/database/migration/service/CategoryProductServiceImplTest.java
@@ -2,33 +2,29 @@ package com.database.migration.service;
 
 import com.database.migration.entity.CategoryProduct;
 import com.database.migration.repository.CategoryProductRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.database.migration.util.ResourceNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
-import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 
 @ExtendWith(MockitoExtension.class)
 public class CategoryProductServiceImplTest {
-    @Autowired
+    @Mock
     CategoryProductRepository categoryProductRepository;
 
-    @Autowired
+    @InjectMocks
     CategoryProductServiceImpl categoryProductService;
 
     CategoryProduct CATEGORY_1 = new CategoryProduct(1L, "Sembako", "Sembako");
@@ -43,11 +39,12 @@ public class CategoryProductServiceImplTest {
 
     @Test
     public void findCategoryProductByIdWhenValidIds() {
-    }
+        given(categoryProductRepository.findById(CATEGORY_1.getId())).willReturn(Optional.of(CATEGORY_1));
 
-    @Test
-    public void findCategoryProductByIdWhenInvalidIds() {
+        CategoryProduct categoryProductExpected = categoryProductService
+                .findCategoryProductById(CATEGORY_1.getId());
 
+        assertThat(categoryProductExpected).isNotNull();
     }
 
     @Test
