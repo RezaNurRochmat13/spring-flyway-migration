b17d608f1bbb4da9c80f71d621b7c0d3dab19419
Reja <rezanurrochmat@gmail.com>
2022-08-09 10:34:32 +0700

    add builder on create product dto

--- src/main/java/com/database/migration/entity/dto/CreateProductDto.java
+++ src/main/java/com/database/migration/entity/dto/CreateProductDto.java
@@ -1,13 +1,15 @@
 package com.database.migration.entity.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class CreateProductDto implements Serializable {
     private String name;
     private String price;
--- src/test/java/com/database/migration/service/ProductServiceImplTest.java
+++ src/test/java/com/database/migration/service/ProductServiceImplTest.java
@@ -1,11 +1,29 @@
 package com.database.migration.service;
 
+import com.database.migration.entity.CategoryProduct;
+import com.database.migration.entity.Product;
+import com.database.migration.entity.dto.CreateProductDto;
+import com.database.migration.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceImplTest {
+    @Mock
+    ProductRepository productRepository;
+
+    @InjectMocks
+    ProductServiceImpl productService;
+
+    CategoryProduct CATEGORY_1 = new CategoryProduct(1L, "Sembako", "Sembako");
+    Product PRODUCT_1 = new Product("Beras", "20000", 2, CATEGORY_1.getId());
 
     @Test
     public void findAllProducts() {
